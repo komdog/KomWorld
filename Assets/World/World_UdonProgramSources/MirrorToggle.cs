da4107d2637d26728bd56cf5cc8e61db28bb93d1
komdog <komdogart@gmail.com>
2023-06-17 04:58:24 -0400

    init

---
+++ Assets/World/World_UdonProgramSources/MirrorToggle.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorToggle : UdonSharpBehaviour
+{
+
+    public GameObject mirrorMain;
+    public bool mirrorMainState;
+
+    void Start()
+    {
+        mirrorMain.SetActive(mirrorMainState);
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Toggling Mirror...");
+        mirrorMainState = !mirrorMainState;
+        mirrorMain.SetActive(mirrorMainState);
+    }
+    
+}
---
+++ Assets/World/World_UdonProgramSources/PlayerModSetter.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayerModSetter : UdonSharpBehaviour
+{
+
+    [Header("Player Settings")]
+    [SerializeField] float jumpHeight = 10;
+    [SerializeField] float walkSpeed = 5;
+    [SerializeField] float runSpeed = 10;
+    [SerializeField] float gravityStrength = 1;
+
+    void Start()
+    {
+
+        var playerApi = Networking.LocalPlayer;
+
+        if(playerApi != null)
+        {
+            playerApi.SetJumpImpulse(jumpHeight);
+            playerApi.SetRunSpeed(runSpeed);
+            playerApi.SetWalkSpeed(walkSpeed);
+            playerApi.SetStrafeSpeed(walkSpeed);
+            playerApi.SetGravityStrength(gravityStrength);
+        }
+        
+    }
+
+    
+}
